757015466e5fee70cca42b76c8ee50d0445fe3f6
Adebayo Temitope <adecancode@gmail.com>
2020-08-15 20:12:01 +0100

    Update Post.vue

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -7,13 +7,12 @@
 
       <p class="txt-2">{{ post.body | capitalize }}</p>
     </div>
-    <div class="icon" @click="commentIs = !commentIs">
+    <div class="icon">
       <p class="comment"><svg width="50" height="50" viewBox="0 0 33 33" fill="000" class="q" aria-label="responses"><path clip-rule="evenodd" d="M24.28 25.5l.32-.29c2.11-1.94 3.4-4.61 3.4-7.56C28 11.83 22.92 7 16.5 7S5 11.83 5 17.65s5.08 10.66 11.5 10.66c1.22 0 2.4-.18 3.5-.5l.5-.15.41.33a8.86 8.86 0 0 0 4.68 2.1 7.34 7.34 0 0 1-1.3-4.15v-.43zm1 .45c0 1.5.46 2.62 1.69 4.44.22.32.01.75-.38.75a9.69 9.69 0 0 1-6.31-2.37c-1.2.35-2.46.54-3.78.54C9.6 29.3 4 24.09 4 17.65 4 11.22 9.6 6 16.5 6S29 11.22 29 17.65c0 3.25-1.42 6.18-3.72 8.3z"></path></svg>
       ({{ total }})</p>
     </div>
 
-<transition name="fade">
-    <div class="comments" v-if="commentIs">
+    <div class="comments" >
       <div class="form-row">
         <div class="form-group col-md-6">
           <input type="text" class="form-control" name="name" id="name" v-model="details.name" placeholder="Name" required>
@@ -37,7 +36,6 @@
         </div>
     </section>
     </div>
-</transition>
 </div>
 </template>
 
@@ -149,19 +147,9 @@ hr{
 .comments{
   margin-top: 4rem;
 }
-.fade-enter-active, .fade-leave-active{
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to{
-  opacity: 0;
-}
+
 .icon{
   margin: 2rem 0;
-  cursor: pointer;
-}
-.icon:hover, .icon:active{
-  transition: ease 100ms;
-  fill: #63c063;
 }
 input{
   font-family: 'Poppins', sans-serif;
